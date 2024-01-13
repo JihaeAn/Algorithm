9c900e2821336bb6ea9d02cefd0dc46e76d88b11
JihaeAn <141133731+JihaeAn@users.noreply.github.com>
2024-01-13 16:32:02 +0900

    [level Lv.1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,5 @@
+-- 코드를 입력하세요
+SELECT b.TITLE, b.BOARD_ID, r.REPLY_ID, r.WRITER_ID, r.CONTENTS, to_char(r.CREATED_DATE, 'YYYY-MM-DD')
+from USED_GOODS_BOARD b join USED_GOODS_REPLY r on b.BOARD_ID = r.BOARD_ID
+where to_char(b.CREATED_DATE, 'YYYY-MM') like '2022-10'
+order by r.CREATED_DATE, TITLE; 
