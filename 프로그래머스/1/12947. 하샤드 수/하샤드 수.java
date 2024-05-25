3f627ea3a84fd570d0fc37fd8d36141ebb6ac67f
JihaeAn <141133731+JihaeAn@users.noreply.github.com>
2024-05-25 17:37:20 +0900

    [level 1] Title: 하샤드 수, Time: 5.43 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        String s = x + "";
+            String str [] = s.split("");
+
+            int sum = 0;
+            for(int i = 0; i<str.length; i++){
+                sum += Integer.parseInt(str[i]);
+            }
+
+            if(x % sum != 0) answer = false;
+        return answer;
+    }
+}
