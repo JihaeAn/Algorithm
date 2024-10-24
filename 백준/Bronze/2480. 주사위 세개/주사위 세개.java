f808eece30a1f6f8d98d72033cce8f9543683e4a
안지해 <dkswlgo6615@naver.com>
2024-10-24 09:06:40 +0900

    [Bronze IV] Title: 주사위 세개, Time: 176 ms, Memory: 17664 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -7,17 +7,15 @@ public class Main {
         int b = sc.nextInt();
         int c = sc.nextInt();
 
-        int big = a;
-        if (big < b) big = b;
-        if (big < c) big = c;
-
         if (a == b && a == c) {
             System.out.println(10000 + a * 1000);
         } else if (a == b || b == c) {
             System.out.println(1000 + b * 100);
         } else if (a == c) {
             System.out.println(1000 + a * 100);
-        } else
-            System.out.println(big * 100);
+        } else {
+            int max = Math.max(Math.max(a, b), c);
+            System.out.println(max * 100);
+        }
     }
 }
