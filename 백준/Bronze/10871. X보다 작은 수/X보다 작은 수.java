c315e3d712ae8cf71e74d50632a4ca220fc49ec4
안지해 <dkswlgo6615@naver.com>
2024-12-01 18:05:34 +0900

    [Bronze V] Title: X보다 작은 수, Time: 284 ms, Memory: 17780 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] n = br.readLine().split(" ");
+
+        int N = Integer.parseInt(n[0]);
+        int x = Integer.parseInt(n[1]);
+
+        String[] list = br.readLine().split(" ");
+
+        for (int i = 0; i < N; i++) {
+            if (Integer.parseInt(list[i]) < x) {
+                System.out.print(Integer.parseInt(list[i]) + " ");
+            }
+        }
+    }
+}
