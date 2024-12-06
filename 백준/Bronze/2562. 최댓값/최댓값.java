34d92886553deb3143fb9b01788857b9794e9744
안지해 <dkswlgo6615@naver.com>
2024-12-06 11:38:19 +0900

    [Bronze III] Title: 최댓값, Time: 100 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -7,19 +7,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] arr = new int[9];
+        int max = 0;
+        int n = 0;
 
         for (int i = 0; i < 9; i++) {
             arr[i] = Integer.parseInt(br.readLine());
-        }
 
-        int max = 0;
-        int n = 0;
-        for (int i = 0; i < 9; i++) {
             if (arr[i] > max) {
                 max = arr[i];
                 n = i;
             }
         }
+        
         System.out.println(max);
         System.out.println(n + 1);
     }
