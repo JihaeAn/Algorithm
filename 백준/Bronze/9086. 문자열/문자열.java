fd8b6a62e91b3bf471a17e6908bfd5bc445f4b3b
안지해 <dkswlgo6615@naver.com>
2024-12-07 16:35:52 +0900

    [Bronze V] Title: 문자열, Time: 96 ms, Memory: 14036 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = br.readLine();
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i].charAt(0));
+            System.out.print(arr[i].charAt(arr[i].length() - 1));
+            System.out.println();
+        }
+    }
+}
