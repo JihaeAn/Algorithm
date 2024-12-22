3182d7cb77e131b7250d9d69aa231be35a970f38
안지해 <dkswlgo6615@naver.com>
2024-12-22 17:01:24 +0900

    [Bronze I] Title: 단어 공부, Time: 232 ms, Memory: 20148 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,50 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
 
-        str = str.toUpperCase();
-        char[] ch = str.toCharArray();
+        int[] arr = new int[26];
 
-        Map<Character,Integer> map  = new HashMap<Character,Integer>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = Character.toUpperCase(str.charAt(i));
+            arr[ch - 'A']++;
+        }
 
-        for(char c : ch){
-            if(map.containsKey(c)){
-                Integer value = map.get(c);
-                map.remove(c);
-                map.put(c, ++value);
-            }else{
-                map.put(c,1);
+        int max = 0;
+        char cha = '?';
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                cha = (char) (i + 'A');
+            } else if (arr[i] == max) {
+                cha = '?';
             }
         }
 
-
-        int maxValue = Integer.MIN_VALUE;
-        char maxKey = ' ';
-        boolean isUnique = true;
-
-        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-            int value = entry.getValue();
-            if (value > maxValue) {
-                maxValue = value;
-                maxKey = entry.getKey();
-                isUnique = true;
-            } else if (value == maxValue) {
-                isUnique = false;
-            }
-        }
-
-        // 결과 출력
-        if (isUnique) {
-            System.out.println(maxKey);  // 유일한 최대 값의 키 출력
-        } else {
-            System.out.println("?");  // 중복된 최대 값이면 '?' 출력
-        }
+        System.out.println(cha);
     }
 }
